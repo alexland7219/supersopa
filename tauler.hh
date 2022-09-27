62c19f991540d0fca72338ece50563228ee7e713
alexland7219 <alexland7219@gmail.com>
2022-09-27 12:32:14 +0200

    Added tauler.hh

---
+++ tauler.hh
@@ -0,0 +1,18 @@
+#ifndef TAULER_HH
+#define TAULER_HH
+
+#include <vector>
+
+class Tauler {
+    
+    private:
+
+    int N; // altura o amplada del Tauler
+    vector<vector<char>> tauler; // tauler del Joc
+
+    public:
+
+    Board(int n);
+}
+
+#endif
