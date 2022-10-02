21bbddf1e8ba9a9050ce0cda878ac9cebc737d6e
alexland7219 <alexland7219@gmail.com>
2022-10-02 18:55:12 +0200

    New hash functions. 2 Left

--- diccBloomFilter.cc
+++ diccBloomFilter.cc
@@ -7,7 +7,7 @@ Bloom::Bloom(vector<string>& s){
 }
 
 int Bloom::Hash1(string s){
-    // Polinomic multiplication
+    // Polinomic multiplication with prime coefficient
     // S'observa bona distribució uniforme quan p = 53
     int p = 53;
     uint32_t i = 1;
@@ -20,13 +20,43 @@ int Bloom::Hash1(string s){
     return i;
 }
 
-int Bloom:Hash2(string s){
+int Bloom::Hash2(string s){
     // Multiplication with prime numbers
-    int i = 7;
+    uint32_t i = 7;
     for (int j = 0; j < s.size(); ++j){
         i = (i * 127) + s[j];
         i %= M;
     }
 
     return i;
+}
+
+int Bloom::Hash3(string s){
+    // djb2 by Dan Bernstein http://www.cse.yorku.ca/~oz/hash.html
+    unsigned long hash = 5381;
+    int c;
+    char * p = &s[0];
+
+    while(c = *p++)
+        hash = ((hash << 5) + hash) + c;
+
+    return (hash % M);
+}
+
+int Bloom::Hash4(string s){
+    // PJW hash function https://www.cs.hmc.edu/~geoff/classes/hmc.cs070.200101/homework10/hashfuncs.html
+    char * p = &s[0];
+    unsigned long h = 0, g;
+
+    while(*p){
+
+        h = (h << 4) + *p++;
+        g = h & 0xF000000;
+
+        if (g != 0)
+            h = h ^ (g >> 24);
+            h = h ^ g;
+    }
+
+    return (h % 100000);
 }
