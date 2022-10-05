501f9ec8db0c6bc3a64fec56107dee70db19e9f2
BernatBC <bernat.borrasc@gmail.com>
2022-10-05 09:17:33 +0200

    Remove false postivie words

--- Trie.cc
+++ Trie.cc
@@ -15,14 +15,14 @@ void Trie::setWord(string s, int i) {
     children[s[i] - 'A']->setWord(s, i + 1);
 }
 
-void Trie::findWords(Board& sopa, int i, int j, set<pair<pair<int, int>, string>>& visited, string s) {
+void Trie::findWords(Board& sopa, int i, int j, set<pair<int, int>> visited, string s) {
     int n = sopa.getSize();
-    if (i < 0 or j < 0 or i >= n or j >= n or visited.find(make_pair(make_pair(i, j), s)) != visited.end() or children[sopa.getCasella(i, j) - 'A'] == NULL) return;
+    if (i < 0 or j < 0 or i >= n or j >= n or visited.find(make_pair(i, j)) != visited.end() or children[sopa.getCasella(i, j) - 'A'] == NULL) return;
 
     s.push_back(sopa.getCasella(i, j));
 
     if (children[sopa.getCasella(i, j) - 'A']->is_word) cout << "Found " << s << endl;
-    visited.insert(make_pair(make_pair(i, j), s));
+    visited.insert(make_pair(i, j));
 
     children[sopa.getCasella(i, j) - 'A']->findWords(sopa, i + 1, j, visited, s);
     children[sopa.getCasella(i, j) - 'A']->findWords(sopa, i, j + 1, visited, s);
