5f76b396552c8d024aa2553c920e569cf78d25ca
Rayogenesis <106593298+Rayogenesis@users.noreply.github.com>
2022-10-12 02:15:31 +0200

    Added the algorithms for Sorted Vector
    Same as .cc

--- diccSortedVector.hh
+++ diccSortedVector.hh
@@ -1,12 +1,29 @@
 #ifndef DICCSORTEDVECTOR_HH
 #define DICCSORTEDVECTOR_HH
 
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <set>
+#include "board.hh"
+
 using namespace std;
 
 class SortedVector {
-    
-}
+    private:
+        vector<string> SortedDicc;
+        vector<int> Index;
+
+        void fillIndex(int i, int j, char c);
+
+    public:
+        SortedVector(vector<string>& dicc);
+
+        int checkPrefix(int i, int j, string& s);
+        void localSearch(Board& board, int i, int j, int& ini, int& fin, string& s, set<pair<int, int>>& visitats, set<string>& foundWords);
+        void findWords(Board& TomatoSoup, set<string>& foundWords);
+
+};
 
 #endif
