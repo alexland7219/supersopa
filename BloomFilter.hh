412c3f2ef7e56fd63287b4808a4fefdb2b6d409a
alexland7219 <alexland7219@gmail.com>
2022-10-12 18:19:58 +0200

    Commented h1..6

--- BloomFilter.hh
+++ BloomFilter.hh
@@ -19,7 +19,7 @@ class Bloom {
     uint32_t MP;
     vector<bool> B; // words
     vector<bool> P; // prefixes of words
-    fstream h1, h2, h3, h4, h5, h6;
+    //fstream h1, h2, h3, h4, h5, h6;
 
     int hash1(string s, bool firstModulus);
     int hash2(string s, bool firstModulus);
